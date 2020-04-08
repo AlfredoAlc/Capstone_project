55f94fe81871fc2b72e744418f62f16d959ec9e1
Alfredo Alcántara <alfredo.alcantara.r@gmail.com>
2020-04-08 16:49:18 -0700

    Fix bugs

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 # database_path = "postgres://aar92_22@localhost:5432/agencyDB"
-database_path = os.environ['DATABASE_URL']
+database_path = 'postgres://brzinsrjvqanxj:099037d3051ff596dca879154af40cc2a7b78d02a3c158f93099a8d5682fa300@ec2-18-235-20-228.compute-1.amazonaws.com:5432/d14h8n7lsu2pem'
 
 db = SQLAlchemy()
 
